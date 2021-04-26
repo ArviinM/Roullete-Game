bdc2aa44a42f6e65a55d8809cf9d3ec81ce9d29a
ArviinM <rhenmedina20@gmail.com>
2021-04-26 22:07:20 +0800

    Updated Gitignore and Uploading Files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const {port} = 3000;
+const {port} = require('./config.json');
 
 const app = express();
 
